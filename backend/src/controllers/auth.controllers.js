d65975b411be827bd3e44a841dfcafce4f95e707
Aryan444Bits <branwalaryan71@gmail.com>
2025-09-09 23:09:53 +0530

    Authentication Completed

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,15 +1,112 @@
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require('bcryptjs')
 
 async function registerController(req,res){
+    const {username, password} = req.body;
 
+
+    const existingUser = await userModel.findOne({
+        username
+    })
+
+    if(existingUser){
+        return res.status(409).json({
+            message:"Username Already Exist"
+        })
+    }
+
+
+    const user = await userModel.create({
+        username , 
+        password : await bcrypt.hash(password,10)
+    })
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET)
+
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message:"User Created Successfully",
+        user
+    })
 }
 
 async function loginController(req,res){
+    const {username , password} = req.body;
 
+    const user = await userModel.findOne({
+        username : username
+    })
+
+    if(!user){
+        return res.status(404).json({
+            message:"User Not Found"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:"Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+    res.cookie("token",token,{
+        expires:new Date(Date.now() + 1000*60*60*24*7),
+    })
+
+    res.status(200).json({
+        message:"User Login Successfully",
+        user,
+        id:user._id
+    })
+}
+
+async function userController(req,res){
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorised"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({
+            _id:decoded.id
+        }).select("-password")
+
+        res.status(200).json({
+            message:"User Data Fetched Successfully",
+            user
+        })
+    }
+    catch(err){
+        return res.status(401).json({
+            message:"Unauthorised - Inavalid Token"
+        })
+    }
+}
+
+async function logoutController(req,res){
+    res.clearCookie("token")
+    res.status(200).json({
+        message:"User Logged Out Successfully"
+    })
 }
 
 
 module.exports={
     registerController,
-    loginController
+    loginController,
+    userController,
+    logoutController
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,109 +1,18 @@
+const {registerController , loginController , userController , logoutController} =  require('../controllers/auth.controllers')
+
+
+
 const express = require('express');
-const userModel = require('../models/user.model');
-const jwt = require("jsonwebtoken")
 const router = express.Router();
 
 
-router.post('/register', async(req, res) => {
-    const {username, password} = req.body;
+router.post('/register',registerController)
 
+router.post('/login',loginController)
 
-    const existingUser = await userModel.findOne({
-        username
-    })
+router.get('/user',userController)
 
-    if(existingUser){
-        return res.status(409).json({
-            message:"Username Already Exist"
-        })
-    }
-
-
-    const user = await userModel.create({
-        username , 
-        password
-    })
-
-    const token = jwt.sign({
-        id:user._id
-    },process.env.JWT_SECRET)
-
-    res.cookie('token',token)
-
-    res.status(201).json({
-        message:"User Created Successfully",
-        user
-    })
-})
-
-router.post('/login',async(req,res)=>{
-
-    const {username , password} = req.body;
-
-    const user = await userModel.findOne({
-        username : username
-    })
-
-    if(!user){
-        return res.status(404).json({
-            message:"User Not Found"
-        })
-    }
-
-    const isPasswordValid = password === user.password
-
-    if(!isPasswordValid){
-        return res.status(401).json({
-            message:"Invalid Password"
-        })
-    }
-
-    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-
-    res.cookie("token",token,{
-        expires:new Date(Date.now() + 1000*60*60*24*7),
-    })
-
-    res.status(200).json({
-        message:"User Login Successfully",
-        user
-    })
-})
-
-router.get('/user',async(req,res)=>{
-    const {token} = req.cookies;
-
-    if(!token){
-        return res.status(401).json({
-            message:"Unauthorised"
-        })
-    }
-
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-
-        const user = await userModel.findOne({
-            _id:decoded.id
-        }).select("-password")
-
-        res.status(200).json({
-            message:"User Data Fetched Successfully",
-            user
-        })
-    }
-    catch(err){
-        return res.status(401).json({
-            message:"Unauthorised - Inavalid Token"
-        })
-    }
-})
-
-router.get('/logout',(req,res)=>{
-    res.clearCookie("token")
-    res.status(200).json({
-        message:"User Logged Out Successfully"
-    })
-})
+router.get('/logout',logoutController)
 
 
 module.exports = router
