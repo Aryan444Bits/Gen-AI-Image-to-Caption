7ee2ae0d912b9367b8df130d8366cbb501af5cb7
Aryan444Bits <branwalaryan71@gmail.com>
2025-09-11 00:27:55 +0530

    Updated code

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,6 @@
+const postModel = require('../models/post.model')
+
+
+async function createPostController(req,res){
+    
+}
---
+++ backend/src/routes/post.routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+const authMiddleware = require('../middleware/auth.middleware')
+const multer = require('multer')
+
+const upload = multer({ storage: multer.memoryStorage() })
+
+router.post('/', // api pe request aai
+    authMiddleware, // check the user verified or not then req.user = user and controller pe send kr dega data
+
+    upload.single("image"),
+    createPostController
+)
+
+
+module.exports = router
