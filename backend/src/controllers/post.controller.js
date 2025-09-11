470e47d68b3f4ae98417474e42dbf00f799c2489
Aryan444Bits <branwalaryan71@gmail.com>
2025-09-12 00:23:14 +0530

    Caption by AI

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,6 +1,25 @@
 const postModel = require('../models/post.model')
+const generateCaption = require('../service/ai.service')
 
 
 async function createPostController(req,res){
-    
+    const file = req.file;
+
+    // console.log("file recived : ",file);
+
+    const base64Image = new Buffer.from(file.buffer).toString('base64');
+
+    // console.log("Base64 Image", base64Image);
+
+    const caption = await generateCaption(base64Image);
+
+    res.json({
+        caption
+    })
+}
+
+
+
+module.exports = {
+    createPostController
 }
